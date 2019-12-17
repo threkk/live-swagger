a7d49a792f7723dd51cff7ce3b87858cf913ee41
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2019-12-17 01:14:30 +0100

    feat: First working version of the file watcher with SSE.
    It is able to send events with the updated file that will be caught by
    the listener.

--- server.ts
+++ server.ts
@@ -1,39 +1,95 @@
 import express = require('express')
-import { join } from 'path'
+import { join, resolve } from 'path'
+import { watch, FSWatcher, readFileSync } from 'fs'
+import { Application } from 'express'
+import { safeLoad, FAILSAFE_SCHEMA } from 'js-yaml'
+import { EOL } from 'os'
 
-interface watcherProps {
-  port: number
-  file: string
-}
+// Constants
+const EVT_RENAME = 'rename'
+const EVT_CHANGE = 'change'
+const EVT_UPDATE = 'update'
+const MSG_ERROR = 'invalid_file'
 
-function watcher(props: watcherProps) {
+// Helper functions
+const evtId = (_: any, id: number): string => `id: ${id}${EOL}`
+const evtType = (_: any, type: string): string => `event: ${type}${EOL}`
+const evtData = (_: any, data: string): string => `data: ${data}${EOL}`
+
+// Application
+function createWatcherServer(target: string): Application {
   const app = express()
-
   app.use(express.static(join(__dirname, 'client', 'build')))
-  app.get('/events', (req, res) => {
-    // Infinite polling
-    // req.socket.setTimeout(Infinity)
+
+  let watcher: FSWatcher | null = null
+
+  // Events.
+  app.get('/events', (_, res) => {
+    let evtCounter: number = 0
+    watcher = watch(target, (event, filename) => {
+      evtCounter++
+      // TODO: Add some debugging console.log(counter, event, filename)
+      if (event === EVT_RENAME) {
+        // TODO: Fill. Add also event for moving files.
+        console.log(`New file name: ${filename}`)
+      } else if (event === EVT_CHANGE) {
+        // TODO: This should use the filename, not a workaround.
+        const content = readFileSync(target, 'utf-8')
+
+        res.write(evtId`${evtCounter}`)
+        res.write(evtType`${EVT_UPDATE}`)
+        try {
+          const output = JSON.stringify(JSON.parse(content)).trim()
+          res.write(evtData`${output}`)
+        } catch {
+          try {
+            const output = JSON.stringify(
+              safeLoad(content, { json: true, schema: FAILSAFE_SCHEMA })
+            ).trim()
+            res.write(evtData`${output}`)
+          } catch {
+            res.write(evtData`${MSG_ERROR}`)
+          }
+        }
+
+        res.write(EOL)
+      } else {
+        console.log('weird flex but ok')
+      }
+    })
 
     res.writeHead(200, {
       'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache',
       Connection: 'keep-alive'
     })
-    res.write('\n')
+
+    res.write(EOL)
   })
 
+  // API.
   app.get('/api', (_, res) => {
-    res.sendFile(join(__dirname, 'examples', 'swagger.json'))
+    // TODO check if YAML, JSON and valid.
+    res.sendFile(resolve(join(__dirname, target)))
   })
 
+  // Client side routing.
   app.get('/*', (_, res) => {
-    res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(resolve(join(__dirname, 'client', 'build', 'index.html')))
   })
 
-  app.listen(props.port)
+  const shutdown = () => {
+    if (watcher != null) {
+      watcher.close()
+    }
+  }
+
+  // TODO: on ENOENT, ECONNREF
+  app.on('SIGTERM', shutdown)
+  app.on('SIGINT', shutdown)
+
+  return app
 }
 
-watcher({
-  port: 9000,
-  file: './examples/swagger.json'
-})
+// TODO: This should go in a CLI.
+createWatcherServer('./examples/swagger.json').listen(9000)
